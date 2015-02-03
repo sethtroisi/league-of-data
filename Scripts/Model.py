afc5aab3b3e0a63965b28da974b7056a77692397
Seth Troisi <SethTroisi@google.com>
2015-02-02 22:32:11 -0800

    implimented suggestions from Rafael

--- Scripts/Model.py
+++ Scripts/Model.py
@@ -24,17 +24,14 @@ def loadOutputFile():
     gameFeatures = parsed['features']
     intVersion = []
     for feature in gameFeatures:
-      if feature == True:
-        intVersion.append(1)
-      elif feature == False:
-        intVersion.append(-1)
-      else:
-        intVersion.append(feature)
+      intVersion.append(1 if feature else 0)
 
-    intVersion = intVersion[:2] 
-
+    intVersion = intVersion[:2]
     features.append(intVersion)
-    goals.append(-1 + 2 * parsed['goal'])
+
+    goal = parsed['goal']
+    assert goal in (True, False)
+    goals.append(1 if goal else 0)
 
   f.close()
 
@@ -60,17 +57,18 @@ def trainModel(trainingFeatures, trainingGoals, testFeatures):
   #       fit_intercept=True, l1_ratio=0.15, learning_rate='optimal',
   #       loss='hinge', n_iter=2, n_jobs=1, penalty='l2', power_t=0.5,
   #       random_state=None, shuffle=False, verbose=True, warm_start=False)
-  clf = SGDClassifier(loss="hinge", penalty="l2", n_iter=20, verbose=False)
+  clf = SGDClassifier(loss="log", penalty="l2", n_iter=20, verbose=True)
 
   clf.fit(trainingFeatures, trainingGoals)
 
   print (clf.coef_)
-  print (clf.intercept_)
+  print ("intercept: {:4.3f}, TrueProp: {:3.1f}%".format(
+      clf.intercept_[0], 100 * trainingGoals.count(True) / len(trainingGoals)))
   print ()
   #print (clf.predict_proba(testFeatures))
   #print (clf.decision_function(testFeatures))
 
-  return clf.predict(testFeatures)
+  return clf.predict_proba(testFeatures)
 
 
 trainFeatures, trainGoals, testFeatures, testGoals = loadOutputFile()
@@ -84,8 +82,9 @@ print ()
 
 modelGoals = trainModel(trainFeatures, trainGoals, testFeatures)
 
-correctPredictions = [modelGuess == testResult
+correctPredictions = [(modelGuess[1] > 0.5) == testResult
     for modelGuess, testResult in zip(modelGoals, testGoals)]
+
 corrects = correctPredictions.count(True)
 testSamples = len(correctPredictions)
 
@@ -93,5 +92,5 @@ print ("Correctness: {}/{} = {:2.1f}".format(
   corrects, testSamples, 100 * corrects / testSamples))
 
 print ("Predict A: {}, B: {}".format(
-    len([i for i in modelGoals if i == True]),
-    len([i for i in modelGoals if i == False])))
+    len([i for i in modelGoals if i[0] > 0.5]),
+    len([i for i in modelGoals if i[1] > 0.5])))
