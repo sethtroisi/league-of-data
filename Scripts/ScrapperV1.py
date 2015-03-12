ed28b350cf90682c3a0db911ef2200dfc21d3b2c
Seth Troisi <SethTroisi@google.com>
2015-03-12 01:12:00 -0700

    ScrapperV1 is now walking the graph

--- Scripts/ScrapperV1.py
+++ Scripts/ScrapperV1.py
@@ -1,6 +1,7 @@
 import json
-import urllib.request
+import random
 import time
+import urllib.request
 
 # TODO(riechelp): convert all * imports to named imports.
 from Util import *
@@ -42,7 +43,7 @@ def getSummonerId(names):
   joinedNames = ','.join(names)
   apiPath = apiFormat.format(summonerNames = joinedNames)
   url = buildUrl(apiPath)
-  time.sleep(0.5)
+  time.sleep(1)
 
   parsed = getParsedResponse(url)
 
@@ -56,29 +57,18 @@ def getMatchHistory(summonerId):
   apiFormat = 'na/v2.2/matchhistory/{summonerId}'
   apiPath = apiFormat.format(summonerId = summonerId)
   url = buildUrl(apiPath)
-  time.sleep(0.5)
+  time.sleep(1)
 
-  parsed = getParsedResponse(url)
-  return parsed
-
-
-#summonerIds = getSummonerId(['inkaruga', 'kingvash'])
-#writeJsonFile('example-getSummonerId', summonerIds)
-summonerIds = loadJsonFile('example-getSummonerId')
-firstId = min(summonerIds.values())
-
-#history = getMatchHistory(firstId)
-#writeJsonFile('example-getMatchHistory', history)
-history = loadJsonFile('example-getMatchHistory')
+  return getParsedResponse(url)
 
 
 def getMatch(matchId):
   apiFormat= 'na/v2.2/match/{matchId}'
   apiPath = apiFormat.format(matchId = matchId)
   url = buildUrl(apiPath)
-  time.sleep(0.5)
+  time.sleep(1)
 
-  parsed = getParsedResponse(url)
+  return getParsedResponse(url)
 
 
 def getChampIds():
@@ -103,3 +93,91 @@ def getChampIds():
   print (sorted(champMap.items()))
   print ("{} champions found".format(len(champMap)))
   return champMap
+
+
+def getSummonerMatches(summonerId):
+  # TODO(sethtroisi): Use rankedQueues query param.
+  history = getMatchHistory(summonerId)
+  writeJsonFile('example-getMatchHistory', history)
+  #history = loadJsonFile('example-getMatchHistory')
+
+  matches = history['matches']
+  assert len(matches) > 0
+
+  fellowSummoners = {}
+  fullMatches = {}
+  for match in matches[:2]:
+    matchId = match['matchId']
+    queueType = match['queueType']
+    region = match['region']
+    season = match['season']
+
+    # TODO(sethtroisi): count number of filtered games (by filter).
+    if queueType != 'RANKED_SOLO_5x5':
+      continue
+
+    if region != 'NA':
+      continue
+
+    if season != 'SEASON2015':
+      continue
+
+    print ("fetching/saving match (id: {})".format(matchId))
+    fullMatch = getMatch(matchId)
+    writeJsonFile('example-getMatch-{}'.format(matchId), fullMatch)
+    #fullMatch = loadJsonFile('example-getMatch-{}'.format(matchId))
+    fullMatches[matchId] = fullMatch
+
+    # Seems to only return information on self? duo?
+    #otherParticipants = match['participantIdentities']
+    otherParticipants = fullMatch['participantIdentities']
+    for participant in otherParticipants:
+      player = participant['player']
+      summonerId = player['summonerId']
+      summonerName = player['summonerName']
+      fellowSummoners[summonerName] = summonerId
+
+
+
+  return fullMatches, fellowSummoners
+
+
+def main():
+  # Id -> Name (all seen summoners)
+  summoners = {}
+
+  # Id -> Name (not yet processed) (only add if not in summoners)
+  unvisited = {}
+
+  # MatchId -> Match.
+  matches = {}
+
+  seedNames = ['inkaruga', 'kingvash']
+  #seedIds = getSummonerId(seedName)
+  #writeJsonFile('example-getSummonerId', seedIds
+  seedIds = loadJsonFile('example-getSummonerId')
+
+  for name, sumId in seedIds.items():
+    summoners[sumId] = name
+    unvisited[sumId] = name
+
+  while len(matches) < 15:
+    newId = random.choice(list(unvisited.keys()))
+    newName = unvisited[newId]
+    # Remove from the list of unprocessed
+    del unvisited[newId]
+
+    print ("Exploying {} ({}) ({} summoners {} visited {} games)".format(
+        newName, newId, len(summoners), len(unvisited), len(matches)))
+
+    newMatches, fellowSummoners = getSummonerMatches(newId)
+
+    matches.update(newMatches)
+    for fName, fId in fellowSummoners.items():
+      if fId not in summoners:
+        summoners[fId] = fName
+        unvisited[fId] = fName
+
+
+if __name__ == "__main__":
+  main()
