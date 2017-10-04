a6dc5c1f558844101459b28c37b1de674bc3c823
Seth Troisi <braintwo@gmail.com>
2017-10-03 23:30:31 -0700

    new photo and better utils

--- Scripts/utils/ClearTmp.sh
+++ Scripts/utils/ClearTmp.sh
@@ -3,7 +3,7 @@ echo "older than $time minutes"
 
 if compgen -G "/tmp/tmp-tf-lol/exploring/*"; then
     echo "deleting some file"
-    find /tmp/tmp-tf-lol/exploring/* -type d -cmin "+$time" -exec rm -rf {} \;
+    find /tmp/tmp-tf-lol/exploring/* -type d -cmin "+$time" -prune -exec rm -rf {} \;
 else
     echo "No files to delete"
 fi
